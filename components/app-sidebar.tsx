673701b395bd645c85297f3409152d7c5c2a922f
AtqiyaHaydar <ashford1524@gmail.com>
2024-11-11 18:16:20 +0700

    feat(components): add sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent className="bg-[#5a56f4]">
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-white poppins-bold text-xl">Medify Chatbot</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu className="mt-4">
+              {items.map((item: any) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild className="text-white hover:bg-white/25 hover:text-white">
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}

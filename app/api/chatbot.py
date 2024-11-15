f1c551f545d76424b011742656aa08c65c11e30f
Valentino Triadi <13522164@std.stei.itb.ac.id>
2024-11-15 11:41:50 +0700

    feat: integrate

--- app/api/chatbot.py
+++ app/api/chatbot.py
@@ -1,4 +1,26 @@
-def chatbotResponse(prompt):
-  response = prompt # Intinya proses si prompt jadi response terus return aja
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
-  return response
+def load_model():
+    model_path = "maull04/chatbot_gpt2_healthcaremagic100"
+    tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
+    model = AutoModelForCausalLM.from_pretrained(model_path)
+    
+    # Menambahkan pad_token jika tidak ada
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        model.resize_token_embeddings(len(tokenizer))
+
+    text_gen_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, device=0)
+    return text_gen_pipeline
+
+text_gen_pipeline = load_model()
+
+def chatbotResponse(prompt):
+    formatted_prompt = (
+      "Instruction: If you are a doctor, please answer the medical questions based on the patient's description.\n"
+      f"Input: {prompt}\nResponse:"
+    )
+    response = text_gen_pipeline(formatted_prompt, max_length=512, do_sample=True, num_return_sequences=1, truncation=False)
+    generated_text = response[0]['generated_text'].split("Response:")[-1].strip()
+
+    return generated_text

58944775009f9d7793b3004314f5227e9a504f05
Maull09 <maulanafirdaus63@gmail.com>
2024-11-16 16:01:15 +0700

    feat: change model

--- app/api/chatbot.py
+++ app/api/chatbot.py
@@ -2,14 +2,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from deep_translator import GoogleTranslator
 
 def load_model():
-    model_name = "maull04/chatbot_gpt2_healthcaremagic100k"
+    model_name = "maull04/biogpt_finetuning"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
     
     # Menambahkan pad_token jika tidak ada
     if tokenizer.pad_token is None:
-        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-        model.resize_token_embeddings(len(tokenizer))
+        tokenizer.pad_token = tokenizer.eos_token
 
     text_gen_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, device=0)
     return text_gen_pipeline
@@ -28,7 +27,7 @@ def chatbotResponse(prompt):
       "Instruction: If you are a doctor, please answer the medical questions based on the patient's description.\n"
       f"Input: {prompt}\nResponse:"
     )
-    response = text_gen_pipeline(formatted_prompt, max_length=512, do_sample=True, num_return_sequences=1, truncation=False)
+    response = text_gen_pipeline(formatted_prompt, max_length=256, do_sample=True, num_return_sequences=1, truncation=True)
     generated_text = response[0]['generated_text'].split("Response:")[-1].strip()
 
     generated_text = idtranslator.translate(generated_text)
